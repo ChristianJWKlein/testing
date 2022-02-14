46a0258d2dd82e9973439eba394e03cb79ac9b8b
Christian John William Klein <cjwk@Christians-MacBook-Air.local>
2022-02-14 15:03:11 -0500

    testing wrong user input

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const sum = (a, b) => {
+  if (!(a && b)) {
+    //to prevent a null or undefined input
+    //The logic required having both a and b.. not just a or b.
+    return "Invalid Input";
+  }
   return a + b;
 };
 
--- index.test.js
+++ index.test.js
@@ -13,4 +13,17 @@ describe("Should contain tests for sum", () => {
     //Assert
     expect(actual).toBe(expectedResult);
   });
+
+  it("should return an error for invalid inputs", () => {
+    //Arrange
+    const a = null;
+    const b = 6;
+    const expectedResult = "Invalid Input";
+
+    //Act
+    const actual = sum(a, b);
+
+    //Assert
+    expect(actual).toBe(expectedResult);
+  });
 });
